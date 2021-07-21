ffc49c31f090ce598fe3224a14f9aeaa04af8d29
Stefano <piero.quiroz93@gmail.com>
2021-07-21 16:45:47 -0500

    Initial commits server

---
+++ server/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+const cookieParser = require('cookie-parser');
+
+//connect mongoDB with Mongoose
+const connectDB = require('./config/mongoDB.config');
+connectDB()
+
+//middlewares
+app.use(cookieParser())
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//routes
+//app.use('/api', require('./routes/animals.routes'));
+//app.use('/api', require('./routes/users.routes'));
+
+
+app.listen(PORT, ()=>{
+    console.log(` 1 : Server Lock and Loading on PORT: ${PORT} `);
+})
